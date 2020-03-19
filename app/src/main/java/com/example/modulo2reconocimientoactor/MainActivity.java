bf7a6f59befe037bee62aad2b213bf830a405874
Evy <knkem@via.tokyo.jp>
2020-03-19 14:31:57 -0500

    >> basic functionality done, need linting.

--- app/src/main/java/com/example/modulo2reconocimientoactor/MainActivity.java
+++ app/src/main/java/com/example/modulo2reconocimientoactor/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.util.Size;
+import android.view.MotionEvent;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.ImageView;
@@ -21,9 +24,11 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
@@ -65,6 +70,37 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
             }
         };
+
+        View pz = findViewById(R.id.picZone);
+        pz.setOnTouchListener(new View.OnTouchListener() {
+            @Override //MUST EXTEND IMAGE VIEW ON FUTURE, to allow draw overlay, and easier management of stuff;
+            public boolean onTouch(View view0, MotionEvent motionEvent) {
+                ImageView view = (ImageView) view0;
+                if ( motionEvent.getAction() == motionEvent.ACTION_DOWN){
+                    double x = motionEvent.getX() , y = motionEvent.getY() ;
+                    touchCoords.add( new Point( x , y ) );
+
+                    //ftsot and integrity logging/checking ahead; end funct will just fill static(?as of now local) var touchCoords
+                    double vx = view.getX() , vy = view.getY(),
+                        vtx = view.getTranslationX() , vty = view.getTranslationY(),
+                        vw = view.getWidth(), vh = view.getHeight(), viewTop= view.getTop() , viewLeft =view.getLeft() ;
+
+                    //preprocess points
+                    Size displaySize  = new Size( view.getWidth(), view.getHeight() ) ,
+                            picSize = new Size( view.getDrawable().getIntrinsicWidth() , view.getDrawable().getIntrinsicHeight() );
+                    bmpCoords =  touch2BmpCoords(touchCoords, displaySize , picSize  );
+
+
+
+                    i++;
+                    toOutViewLog("point "+ i + "-- x: "+motionEvent.getX() + "->"+ x + "  y:"+motionEvent.getY() + "->"+y );
+                    evokeMat(baseMat);
+                    Log.d("POINTCLC", "point "+ i + "data (x , y , viewWidth , viewHeigh):        "+ x + " :: " + y + " :: " + vw + " :: " + vh);
+                    //Log.d("POINTADD", "point "+ i + "-- x: "+motionEvent.getX() + "  y:"+motionEvent.getY());
+                }
+                return true;//if ret false-> only enters once
+            }
+        });
 
     }
 
@@ -129,16 +165,20 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     //wrapper to invoke cam app
     Mat baseMat;
     static final int REQ_NOOD=1, REQ_GALL = 2;
+
     public void invokeCamera(View view){
         Intent sendNoodsInt = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if(sendNoodsInt.resolveActivity(getPackageManager()) != null){
             startActivityForResult( sendNoodsInt , REQ_NOOD );
         }
     }
-    //doing thumbnail stuff for now, too sleppy
+    //doing thumbnail stuff for now, too sleppy //oops. ..  will this be needed?
     @Override
     protected void onActivityResult( int REQ_COD, int RES_COD, Intent data ){
         if ( REQ_COD == REQ_NOOD &&  RES_COD == RESULT_OK  ) {
+
+            resetPoints();
+
             Bundle extras = data.getExtras();
             Bitmap picBM = (Bitmap) extras.get("data");
             baseMat = new Mat(picBM.getHeight() , picBM.getWidth() , CvType.CV_32SC3);
@@ -149,10 +189,14 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
 
 
+
         }
         if ( REQ_COD == REQ_GALL &&  RES_COD == RESULT_OK  ) {
             Uri selectedImage = data.getData();
             try {
+
+                resetPoints();
+
                 //ImageDecoder.Source imgSrc = ImageDecoder.createSource(  getContentResolver() , selectedImage );
                 Bitmap gallBM = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
                 //Bitmap gallBM = ImageDecoder.decodeBitmap(imgSrc );
@@ -161,6 +205,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
                 baseMat = new Mat();
                 Utils.bitmapToMat(gallBM ,baseMat );
                 evokeMat(baseMat);
+
             } catch (IOException e) {
                 Log.i("TAG", "Some exception " + e);
             }
@@ -179,7 +224,12 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     public void evokeMat(Mat mat){
         ImageView IV = findViewById(R.id.picZone);
         Bitmap temp = Bitmap.createBitmap(mat.width(),mat.height(), Bitmap.Config.ARGB_8888 );
-        Utils.matToBitmap( mat , temp);
+
+        //overlaying points (can also do extending imageView but this seems easier as of now)
+        Mat mat2 = drawOnMat( mat , bmpCoords );
+
+        Utils.matToBitmap( mat2 , temp);
+
         IV.setImageBitmap(temp);
 
     }
@@ -193,6 +243,80 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     public String getSceneName(){
         TextView nameView = findViewById( R.id.nameInp );
         return nameView.getText().toString();
+    }
+
+
+    //temporarily defining aux class point, this is ALSO defined in ocv and android??
+    /*public class Point{
+
+        private double x, y;
+
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+        public void setX(double x) {
+            this.x = x;
+        }
+
+        public void setY(double y) {
+            this.y = y;
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public double getY() {
+            return y;
+        }
+    }*/
+    //temp vars and useful
+    ArrayList<Point > touchCoords = new ArrayList<>();
+    ArrayList<Point > bmpCoords = new ArrayList<>();
+    static int i = 0;
+
+
+    //prob aux method will not be needed?
+    public void updatePoints(View view , Size asindisplaySize ){ //will most likely not even use
+
+    }
+    public void resetPoints(){
+        touchCoords = new ArrayList<>();
+        bmpCoords = new ArrayList<>();
+        i = 0;
+    }
+
+    //expects touchCoords relative to the displays size, NOTE: 0,0 > left, top
+    public ArrayList<Point> touch2BmpCoords(ArrayList<Point> touchCords , Size displaySize  , Size bmpSize){
+        ArrayList<Point> bmpCoords = new ArrayList<>();
+        for  (Point p :touchCords){
+            //note x * matDim / dispDim
+            bmpCoords.add(new Point( p.x  * bmpSize.getWidth() / displaySize.getWidth() , p.y * bmpSize.getHeight() /displaySize.getHeight() ) ) ;
+        }
+        return bmpCoords;
+    }
+
+    public Mat drawOnMat(Mat mat, ArrayList<Point> pointList){
+        //no try catch use at your own risk lol #YOLO
+        Mat retMat = mat.clone();
+        double val[]  = {0,0,0,1};
+        Log.d("point", "sizeofpnt " + pointList.size());
+
+        for (Point p : pointList ){
+            for (int a = -5; a<5 ; a++ ){
+                for (int b = -5 ; b<5;b++ ) {
+                    try {
+                        retMat.put((int) p.y + a, (int) p.x + b, val);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Log.d("putError", e.getMessage());
+                    }
+                }
+            }
+        }
+        return retMat;
     }
 
 }
